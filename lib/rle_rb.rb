826b13c7cf07ecf0644c773287a9dbb5cb8ce88b
Hayden Rouille <hayden@rouille.dev>
2024-06-15 12:39:04 +0100

    Setup Gem

---
+++ lib/rle_rb.rb
@@ -0,0 +1,5 @@
+class RleRb
+  def self.hi
+    puts "Hello world!"
+  end
+end
---
+++ spec/rle_rb_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe RleRb do
+  it "is true" do
+    expect(true).to eq(true)
+  end
+end
