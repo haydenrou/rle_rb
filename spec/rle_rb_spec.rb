2a81fb57540ec11d101c781059c96d4016135c53
Hayden Rouille <hayden@rouille.dev>
2024-07-28 19:59:14 +0300

    Use different characters in specs

--- spec/rle_rb_spec.rb
+++ spec/rle_rb_spec.rb
@@ -2,14 +2,14 @@
 
 RSpec.describe RleRb do
   it "encodes a basic string correctly" do
-    example_string = "aaaabbbcc"
+    example_string = "[aaaabbbcc&4"
     encoding = RleRb.encode(example_string)
-    expect(encoding).to eq("4a3b2c")
+    expect(encoding).to eq("1[4a3b2c1&14")
   end
 
   it "decodes a basic string correctly" do
-    example_string = "4a3b2c"
+    example_string = "1[4a3b2c1&1["
     decoding = RleRb.decode(example_string)
-    expect(decoding).to eq("aaaabbbcc")
+    expect(decoding).to eq("[aaaabbbcc&[")
   end
 end
